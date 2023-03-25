be7ae2ada5aaaf31321bed5eb0a905f7ee896caa
alex lieberman <alex.lieberman182@gmail.com>
2023-03-25 09:50:15 -0700

    commit tree 34d17a247dd3f10b6939e0fbb2fdc3d873cda9e8

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,11 @@
-# Write your code here.
+require 'pry'
+
+katz_deli = []
+
+def line(katz_deli)
+    if katz_deli.length == 0
+        puts "The line is currently empty."
+    else
+        puts katz_deli.length + 1    
+    end    
+end
